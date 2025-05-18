857779a7817dc37f0da9462524d51c8d180144ce
carloscontrolid <carlos.veloso@controlid.com.br>
2025-05-18 16:00:19 -0300

    terraform: alb fix

--- alb-security-groups.tf
+++ alb-security-groups.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "idsecure_loadbalancer_security_group_public" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    security_groups = [aws_security_group.idsecure_ecs_security_group.id]
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   lifecycle {
